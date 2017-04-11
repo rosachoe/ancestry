9936487d3208cfca06e1a75c93d53c4406b4ca08
rosachoe <rosachoe@berkeley.edu>
2017-04-11 00:21:42 -0700

    Initial Commit

---
+++ Cemetery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cemetery
+//
+//  Created by Rosa Choe on 4/11/17.
+//  Copyright © 2017 Rosa Choe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
